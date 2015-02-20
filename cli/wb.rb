5be9ead2871da5bc3c28fc50ff4735dfad8b327f
Hexagonal Architects <butzopower@gmail.com>
2015-02-20 13:30:59 -0800

    Refactor CLI observer

--- cli/wb.rb
+++ cli/wb.rb
@@ -5,19 +5,22 @@ require 'board'
 require 'board_test_support/doubles/fake_team_repo'
 
 class MyCLI < Thor
+  module CLIObserver
+    extend self
+
+    def team_created(team)
+      puts "Created team #{team.name}"
+    end
+  end
+
   desc "create_team", "Interactively create a team"
   def create_team
     team_name = ask("Enter your team name: ")
     Board.create_team(
-        observer: self,
+        observer: CLIObserver,
         team_repo: team_repo,
         attributes: {name: team_name}
     ).execute
-  end
-
-  desc "go away", "whatever"
-  def team_created(team)
-    puts "Created team #{team.name}"
   end
 
   private
