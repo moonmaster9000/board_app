295810f951f40bac62675dc1b244990343d98631
Matt Parker <moonmaster9000@gmail.com>
2015-03-14 10:47:06 -0700

    Move private value class for standup to bottom of use case

--- board/lib/board/use_cases/present_standup_use_case.rb
+++ board/lib/board/use_cases/present_standup_use_case.rb
@@ -1,16 +1,6 @@
 module Board
   module UseCases
     class PresentStandupUseCase
-      module Values
-        class Standup
-          attr_reader :new_faces
-
-          def initialize(new_faces:)
-            @new_faces = new_faces
-          end
-        end
-      end
-
       def initialize(team_id:, new_face_repo:, observer:)
         @observer = observer
         @new_face_repo = new_face_repo
@@ -20,6 +10,16 @@ module Board
       def execute
         standup = Values::Standup.new(new_faces: @new_face_repo.all_by_team_id(@team_id))
         @observer.standup_presented(standup)
+      end
+
+      module Values
+        class Standup
+          attr_reader :new_faces
+
+          def initialize(new_faces:)
+            @new_faces = new_faces
+          end
+        end
       end
     end
   end
