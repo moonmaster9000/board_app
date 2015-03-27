947402d034bbd6d3762817e309387653912fad01
Matt Parker <moonmaster9000@gmail.com>
2015-03-26 20:25:37 -0700

    Refactoring to Multiple Inheritence, the Ruby Way

--- persistence/lib/persistence/repos/help_repo.rb
+++ persistence/lib/persistence/repos/help_repo.rb
@@ -1,21 +1,19 @@
 require "board/entities/help"
 require "persistence/private/help"
 require "persistence/repos/entity_repo"
+require "persistence/repos/lookup_by_team"
 
 module Persistence
   module Repos
     class HelpRepo < EntityRepo
+      include LookupByTeam
+
       def entity_class
         Board::Entities::Help
       end
 
       def table_class
         Private::Help
-      end
-
-      def all_by_team_id(team_id)
-        entity_records = table_class.where(team_id: team_id)
-        entity_records.map { |entity_record| entity_from_record(entity_record) }
       end
     end
   end
--- persistence/lib/persistence/repos/new_face_repo.rb
+++ persistence/lib/persistence/repos/new_face_repo.rb
@@ -1,3 +1,4 @@
+require "persistence/repos/lookup_by_team"
 require "board/entities/new_face"
 require "persistence/private/new_face"
 require "persistence/repos/entity_repo"
@@ -5,17 +6,14 @@ require "persistence/repos/entity_repo"
 module Persistence
   module Repos
     class NewFaceRepo < EntityRepo
+      include LookupByTeam
+
       def entity_class
         Board::Entities::NewFace
       end
 
       def table_class
         Private::NewFace
-      end
-
-      def all_by_team_id(team_id)
-        entity_records = table_class.where(team_id: team_id)
-        entity_records.map { |entity_record| entity_from_record(entity_record) }
       end
     end
   end
