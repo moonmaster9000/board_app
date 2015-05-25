585450d7a89cf91fe2823c05cee9ac045621b0bd
Matt Parker <moonmaster9000@gmail.com>
2015-05-25 19:46:48 -0400

    resolving open/closed violation in PresentWhiteboardUseCase

--- board/lib/board/use_cases/present_whiteboard_use_case.rb
+++ board/lib/board/use_cases/present_whiteboard_use_case.rb
@@ -3,20 +3,39 @@ require "board/values/whiteboard"
 module Board
   module UseCases
     class PresentWhiteboardUseCase
+      class << self
+        def collectors
+          @collector ||= []
+        end
+
+        def add_collector(collector)
+          collectors << collector
+        end
+      end
+
       def initialize(team_id:, observer:, repo_factory:)
         @repo_factory = repo_factory
         @observer = observer
         @team_id = team_id
+        @items = {}
+      end
+      
+      def add_items(item_name, items)
+        @items[item_name] = items
       end
 
       def execute
-        @observer.whiteboard_presented(
-          Board::Values::Whiteboard.new(
-            new_faces:  @repo_factory.new_face_repo.unarchived_by_team_id(@team_id),
-            helps:      @repo_factory.help_repo.unarchived_by_team_id(@team_id),
-          )
-        )
+        self.class.collectors.each do |collector|
+          collector.call(repo_factory: @repo_factory, team_id: @team_id, whiteboard_items: self)
+        end
+
+        @observer.whiteboard_presented(Board::Values::Whiteboard.new(@items))
       end
     end
   end
+end
+
+
+Dir[File.join(__dir__, "whiteboard_item_collectors", "**", "*.rb")].each do |collector|
+  require collector
 end
---
+++ board/lib/board/use_cases/whiteboard_item_collectors/collect_helps.rb
@@ -0,0 +1,6 @@
+collect_helps_for_whiteboard = -> (repo_factory:, team_id:, whiteboard_items:) do
+  unarchived_helps = repo_factory.help_repo.unarchived_by_team_id(team_id)
+  whiteboard_items.add_items(:helps, unarchived_helps)
+end
+
+Board::UseCases::PresentWhiteboardUseCase.add_collector(collect_helps_for_whiteboard)
