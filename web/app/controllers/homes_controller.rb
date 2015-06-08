6a88b10d20256b1cc52ab2b0e6084d535ed2bc5b
Matthew Kane Parker <mparker@pivotallabs.com>
2015-06-08 16:07:30 -0400

    WIP

--- web/app/controllers/homes_controller.rb
+++ web/app/controllers/homes_controller.rb
@@ -1,5 +1,12 @@
 class HomesController < ApplicationController
   def index
+    Board.present_whiteboards(
+      observer: self,
+      whiteboard_repo: whiteboard_repo,
+    ).execute
+  end
 
+  def whiteboards_presented(whiteboards)
+    @whiteboards = whiteboards
   end
 end
