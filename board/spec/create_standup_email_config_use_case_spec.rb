749565131922e9620857802af6d1e96999490db5
Matt Parker <moonmaster9000@gmail.com>
2015-07-12 12:55:31 -0400

    Simplifying test of create_standup_email_config use case.

--- board/spec/create_standup_email_config_use_case_spec.rb
+++ board/spec/create_standup_email_config_use_case_spec.rb
@@ -8,66 +8,50 @@ describe "USE CASE: configuring a whiteboard for standup emails" do
   include CommonAssertions
   include BoardTestDSL
 
-  context "Given a whiteboard exists" do
+  before do
+    @whiteboard = create_whiteboard
+  end
+
+  describe "invalid data scenarios" do
+    it "from_address required" do
+      create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui, from_address: nil)
+
+      assert_observer_got_one_error(gui, :from_address, :required)
+    end
+
+    it "to_address required" do
+      create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui, to_address: nil)
+      assert_observer_got_one_error(gui, :to_address, :required)
+    end
+
+    it "whiteboard_id required" do
+      create_standup_email_config(whiteboard_id: nil, observer: gui)
+
+      assert_observer_got_one_error(gui, :whiteboard_id, :required)
+    end
+  end
+
+  describe "invalid state scenarios" do
+    it "only one config per whiteboard allowed" do
+      create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui)
+      create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui)
+      expect(gui.spy_standup_email_config_already_exists).to be(true)
+    end
+  end
+
+  describe "Given valid data" do
+    let(:standup_email_config_attributes) { valid_standup_email_config_attributes }
+
     before do
-      @whiteboard = create_whiteboard
+      create_standup_email_config({whiteboard_id: @whiteboard.id, observer: gui}.merge standup_email_config_attributes)
     end
 
-    context "And a standup email config already exists for that whiteboard" do
-      before do
-        create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui)
-      end
-
-      context "When I try to create another standup email config" do
-        before do
-          create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui)
-        end
-
-        specify "Then the observer should receive a 'standup_email_config_already_exists' message" do
-          expect(gui.spy_standup_email_config_already_exists).to be(true)
-        end
-      end
+    it "sends a standup email config back to the gui with the requested attributes" do
+      expect(gui.spy_created_standup_email_config.attributes).to include(standup_email_config_attributes)
     end
 
-    context "But that whiteboard has no standup email config" do
-      context "When the standup email attributes are invalid" do
-        context "because the 'from_address' is blank" do
-          it "informs the observer that a 'from_address' is required" do
-            create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui, from_address: nil)
-            assert_observer_got_one_error(gui, :from_address, :required)
-          end
-        end
-
-        context "because the 'to_address' is blank" do
-          it "informs the observer that a 'to_address' is required" do
-            create_standup_email_config(whiteboard_id: @whiteboard.id, observer: gui, to_address: nil)
-            assert_observer_got_one_error(gui, :to_address, :required)
-          end
-        end
-
-        context "because I don't give it a whiteboard_id" do
-          it "informs the observer that a whiteboard_id is required" do
-            create_standup_email_config(whiteboard_id: nil, observer: gui)
-            assert_observer_got_one_error(gui, :whiteboard_id, :required)
-          end
-        end
-      end
-
-      context "When the attributes are valid" do
-        let(:standup_email_config_attributes) { valid_standup_email_config_attributes }
-
-        before do
-          create_standup_email_config({whiteboard_id: @whiteboard.id, observer: gui}.merge(standup_email_config_attributes))
-        end
-
-        it "sends a standup email config back to the gui with the requested attributes" do
-          expect(gui.spy_created_standup_email_config.attributes).to include(standup_email_config_attributes)
-        end
-
-        it "doesn't send any validation errors" do
-          expect(gui.spy_validation_errors).not_to be
-        end
-      end
+    it "doesn't send any validation errors" do
+      expect(gui.spy_validation_errors).not_to be
     end
   end
 
