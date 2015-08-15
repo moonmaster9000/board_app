a19715bae7f2be9f46580bd038f8ddf3e10e207b
Matt Parker <moonmaster9000@gmail.com>
2015-08-15 09:40:15 -0400

    cleaning up language in session contract

--- authenticated_board/lib/authenticated_board_test_support/contract_tests/session_contract.rb
+++ authenticated_board/lib/authenticated_board_test_support/contract_tests/session_contract.rb
@@ -1,13 +1,13 @@
 def assert_works_like_session(session_factory:)
-  describe "Session Repo" do
-    specify "it keeps track of whether or not a user is logged in" do
-      repo = session_factory.call
+  describe "Session" do
+    it "keeps track of whether or not a user is logged in" do
+      session = session_factory.call
 
-      expect(repo).not_to be_logged_in
+      expect(session).not_to be_logged_in
 
-      repo.log_in
+      session.log_in
 
-      expect(repo).to be_logged_in
+      expect(session).to be_logged_in
     end
   end
 end
