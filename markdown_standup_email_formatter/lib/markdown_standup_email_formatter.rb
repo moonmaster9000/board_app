baf3037bddff40540253d3e7a280b1478e0afb3b
Matt Parker <moonmaster9000@gmail.com>
2015-08-15 09:29:42 -0400

    Simplified markdown standup email formatter
    * takes advantage of the "tell, don't ask" API standup.available_items

--- markdown_standup_email_formatter/lib/markdown_standup_email_formatter.rb
+++ markdown_standup_email_formatter/lib/markdown_standup_email_formatter.rb
@@ -3,56 +3,20 @@ require "pathname"
 
 class MarkdownStandupEmailFormatter
   def format_email(standup)
-    template = File.read(File.join(__dir__, "email.erb"))
-    item_categories = ItemCategories.new(standup.items)
-    ERB.new(template).result binding
+    email_template = File.read(File.join(__dir__, "email.erb"))
+    ERB.new(email_template).result binding
   end
 
   def content_type
     "text/html"
   end
 
-  class ItemCategories
-    def initialize(categories)
-      @categories = categories
-    end
+  def render_items(item_name, item_values)
+    ERB.new(File.read(File.join(__dir__, "#{item_name}.erb"))).result binding
+  end
 
-    def each
-      @categories.each do |category, items|
-        if items.empty?
-          yield NullItems.new
-        else
-          yield Items.new(category, items)
-        end
-      end
-    end
-
-    class NullItems
-      def render
-        ""
-      end
-    end
-
-    class Items
-      attr_reader :name, :values
-
-      def initialize(items_name, items_values)
-        @name = items_name
-        @values = items_values
-      end
-
-      def render
-        path = Pathname.new(File.join(__dir__, "#{name}.erb"))
-
-        ERB.new(File.read(path)).result binding
-      end
-
-      private
-
-      def markdown_to_html(text)
-        require "github/markdown"
-        GitHub::Markdown.render_gfm(text)
-      end
-    end
+  def markdown_to_html(text)
+    require "github/markdown"
+    GitHub::Markdown.render_gfm(text)
   end
 end
