e5dc77a1c7b6addc328528740d4dfb9b2e9a69be
Matt Parker <moonmaster9000@gmail.com>
2015-08-30 09:55:15 -0400

    Wiring up update_interesting use case

--- plugins/web/app/controllers/interestings_controller.rb
+++ plugins/web/app/controllers/interestings_controller.rb
@@ -4,24 +4,76 @@ class InterestingsController < ApplicationController
     @errors = {}
   end
 
+  def edit
+    @errors = {}
+
+    use_case_factory.read_interesting(
+      repo_factory: repo_factory,
+      interesting_id: params[:id],
+      observer: EditObserver.new(self),
+      session: app_session,
+    ).execute
+  end
+
+  def update
+    use_case_factory.update_interesting(
+      repo_factory: repo_factory,
+      interesting_id: params[:id],
+      attributes: params[:interesting].symbolize_keys,
+      session: app_session,
+      observer: UpdateObserver.new(self),
+    ).execute
+  end
+
+  class UpdateObserver < SimpleDelegator
+    def interesting_updated(*)
+      flash[:notice] = t('interestings.update_success_flash_message')
+      redirect_to whiteboard_path(params[:whiteboard_id])
+    end
+
+    def validation_failed(errors)
+      set_errors(errors)
+      set_interesting(Interesting.new(params[:interesting]))
+
+      render action: :edit
+    end
+  end
+
+  def set_interesting(interesting)
+    @interesting = interesting
+  end
+
+  class EditObserver < SimpleDelegator
+    def interesting_read(interesting)
+      set_interesting(Interesting.new(interesting.attributes))
+    end
+  end
+
   def create
     use_case_factory.create_interesting(
       whiteboard_id: params[:whiteboard_id],
       interesting_repo: interesting_repo,
       attributes: params[:interesting].symbolize_keys,
-      observer: self,
+      observer: CreateObserver.new(self),
       session: app_session,
     ).execute
   end
 
-  def interesting_created(interesting)
-    redirect_to whiteboard_path(params[:whiteboard_id])
+  class CreateObserver < SimpleDelegator
+    def interesting_created(interesting)
+      redirect_to whiteboard_path(params[:whiteboard_id])
+    end
+
+    def validation_failed(errors)
+      set_errors(errors)
+      set_interesting(Interesting.new(params[:interesting]))
+
+      render action: :new
+    end
   end
 
-  def validation_failed(errors)
+  def set_errors(errors)
     @errors = errors
-    @interesting = Interesting.new(params[:interesting])
-    render action: :new
   end
 end
 
