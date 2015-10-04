e52885b47ae4fb01a19fa38c086f6376dc88db79
Matt Parker <moonmaster9000@gmail.com>
2015-10-03 22:09:46 -0400

    Spelling mistake

--- board/spec/support/common_assertions.rb
+++ board/spec/support/common_assertions.rb
@@ -1,7 +1,7 @@
 module CommonAssertions
   def assert_observer_got_one_error(observer, field_name, error)
     expect(observer.spy_validation_errors).to be, "no validation error received, but one was expected"
-    expect(observer.spy_validation_errors.count).to eq(1), "only one validation error was expected, but many were recieved: #{observer.spy_validation_errors}"
+    expect(observer.spy_validation_errors.count).to eq(1), "only one validation error was expected, but many were received: #{observer.spy_validation_errors}"
 
     validation = observer.spy_validation_errors.first
 
